ba22cd6c316d3c66d776dbc25176b3d9ae0dcdc5
Konrad <kmaslowiec@gmail.com>
2021-03-30 07:37:22 +0200

    Navigation Component 0.3 Test Button impl The button directs to GameFragment using NavController

--- app/src/main/java/com/example/android/crossfittrivia/StartFragment.kt
+++ app/src/main/java/com/example/android/crossfittrivia/StartFragment.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class StartFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
+        val rootView: View = inflater.inflate(R.layout.fragment_start, container, false)
+        val startButton : Button = rootView.findViewById(R.id.start_button)
 
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        startButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_startFragment_to_gameFragment)
+        }
+
+        // Inflate the layout for this fragment
+        return rootView
     }
 }
