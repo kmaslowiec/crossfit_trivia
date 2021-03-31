ebfa4832a5d50cd675177e259531c2ffe97dc071
Konrad <kmaslowiec@gmail.com>
2021-03-31 07:30:30 +0200

    Data Binding impl in module Gradle impl in StartFragment.kt MainActivity.kt without binding yet

--- app/src/main/java/com/example/android/crossfittrivia/MainActivity.kt
+++ app/src/main/java/com/example/android/crossfittrivia/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.android.crossfittrivia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/android/crossfittrivia/StartFragment.kt
+++ app/src/main/java/com/example/android/crossfittrivia/StartFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
+import com.example.android.crossfittrivia.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
 
@@ -14,14 +15,21 @@ class StartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val rootView: View = inflater.inflate(R.layout.fragment_start, container, false)
-        val startButton : Button = rootView.findViewById(R.id.start_button)
 
-        startButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_startFragment_to_gameFragment)
-        }
+        /**
+         * Setups data binding for the Fragment
+         */
+        val binding: FragmentStartBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_start, container, false)
+
+        /**
+         * findViewById without Data Binding
+         */
+        /*val rootView: View = inflater.inflate(R.layout.fragment_start, container, false)
+        val startButton : Button = rootView.findViewById(R.id.start_button)*/
+
+        binding.startButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_startFragment_to_gameFragment))
 
         // Inflate the layout for this fragment
-        return rootView
+        return binding.root
     }
 }
