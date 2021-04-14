0344dde71a4ace0ecdbb47b4465980c2a5718670
Konrad <kmaslowiec@gmail.com>
2021-04-14 06:58:56 +0200

    DatBinding impl to EMOM

--- app/src/main/java/com/example/android/crossfittrivia/EmomFragment.kt
+++ app/src/main/java/com/example/android/crossfittrivia/EmomFragment.kt
@@ -5,18 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.android.crossfittrivia.databinding.FragmentEmomBinding
 
 class EmomFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         //Change fragment title
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.emom_title)
 
+        val binding : FragmentEmomBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_emom, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emom, container, false)
+        return binding.root
     }
 }
