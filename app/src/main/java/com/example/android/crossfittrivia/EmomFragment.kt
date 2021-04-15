437f64e51fbc0ca5c861219489881c20b1c989d8
Konrad <kmaslowiec@gmail.com>
2021-04-15 07:24:31 +0200

    Data v. 0.1.5 Enum was not good idea. I will use the list until I impl SQLite

--- app/src/main/java/com/example/android/crossfittrivia/EmomFragment.kt
+++ app/src/main/java/com/example/android/crossfittrivia/EmomFragment.kt
@@ -7,16 +7,17 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.example.android.crossfittrivia.data.QUESTIONS
 import com.example.android.crossfittrivia.data.Question
+import com.example.android.crossfittrivia.data.QuestionsList
 import com.example.android.crossfittrivia.databinding.FragmentEmomBinding
 
 class EmomFragment : Fragment() {
 
+    private var questions: MutableList<Question> = QuestionsList.questions
     lateinit var currentQuestion: Question
     lateinit var answers: MutableList<String>
     private var questionIndex = 0
-    private val numQuestions = Math.min((QUESTIONS.values().size + 1) / 2, 3)
+    private val numQuestions = Math.min((questions.size + 1) / 2, 3)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +28,26 @@ class EmomFragment : Fragment() {
 
         val binding: FragmentEmomBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_emom, container, false)
 
+        randomizeQuestions()
+
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun randomizeQuestions() {
+        questions.shuffle()
+        questionIndex = 0
+        setQuestion()
+    }
+
+    // Sets the question and randomizes the answers.  This only changes the data, not the UI.
+    // Calling invalidateAll on the FragmentGameBinding updates the data.
+    private fun setQuestion() {
+        currentQuestion = questions[questionIndex]
+        // randomize the answers into a copy of the array
+        answers = currentQuestion.answers.toMutableList()
+        // and shuffle them
+        answers.shuffle()
+        //(activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_android_trivia_question, questionIndex + 1, numQuestions)
     }
 }
--- app/src/main/java/com/example/android/crossfittrivia/data/Question.kt
+++ app/src/main/java/com/example/android/crossfittrivia/data/Question.kt
@@ -2,4 +2,5 @@ package com.example.android.crossfittrivia.data
 
 data class Question(var text: String, var answers: List<String>) {
 
+
 }
