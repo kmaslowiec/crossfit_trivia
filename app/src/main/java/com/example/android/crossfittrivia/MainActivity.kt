562c5ddbc91ab2f9c8e4ad00e9751e35c80a7d97
Konrad <kmaslowiec@gmail.com>
2021-07-01 22:53:36 +0200

    Up Button with FragmentContainerView

--- app/src/main/java/com/example/android/crossfittrivia/MainActivity.kt
+++ app/src/main/java/com/example/android/crossfittrivia/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.crossfittrivia
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = this.findNavController(R.id.nav_host_fragment)
-        navController.setGraph(R.navigation.nav_graph)
+        /* That is required to work with FragmentContainerView. Outside on create we can use:
+           val navController = this.findNavController(R.id.nav_host_fragment) */
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
